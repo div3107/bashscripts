3a238e9a626a66c39484f95bd93c07e33074a9fd
Divyanshu Gupta <88083396+div3107@users.noreply.github.com>
2025-02-05 15:43:29 +0530

    Add files via upload

---
+++ name.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#This is a demo program to learn variables....
+author=lostboy
+echo "Hello, My friend welcome $1 to my script"
+echo "This script is written by $author"
---
+++ username.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+read -sp "Enter Your name: " name
+echo -e "\nHii $name, Welcome to my script"
